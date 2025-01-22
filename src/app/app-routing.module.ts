ddc375df581c86c5a154ebfc335083a30e9d265b
shvvkz <viggocasciano0301@gmail.com>
2025-01-22 13:26:07 +0100

    home style quite glitchy, but works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './features/courses/courses.component';
 import { CourseDetailedComponent } from './features/course-detailed/course-detailed.component';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: CoursesComponent },
-  { path: 'courses/:id', component: CourseDetailedComponent }
+  { path: '', component: HomeComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'courses/:id', component: CourseDetailedComponent },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./features/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'admin-dashboard',
+    loadChildren: () =>
+      import('./features/admin-dashboard/admin-dashboard.module').then(
+        (m) => m.AdminDashboardModule
+      ),
+  },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './core/interceptors/auth/auth.interceptor';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './features/courses/courses.component';
@@ -10,6 +12,7 @@ import { MarkdownModule } from 'ngx-markdown';
 import { CourseDetailedComponent } from './features/course-detailed/course-detailed.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './features/home/home.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,8 @@ import { FooterComponent } from './shared/footer/footer.component';
     CoursesComponent,
     CourseDetailedComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +30,9 @@ import { FooterComponent } from './shared/footer/footer.component';
     AppRoutingModule,
     MarkdownModule.forRoot({ loader: HttpClient })
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
