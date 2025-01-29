635470709f38db9082ed511e142cf5e0bdbd44d9
shvvkz <viggocasciano0301@gmail.com>
2025-01-29 14:09:02 +0100

    shared's doc done

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../core/services/auth/auth.service';
 import { Router } from '@angular/router';
 
+/**
+ * Composant représentant l'en-tête de l'application.
+ * Il gère l'affichage de la navigation en fonction de l'état de connexion de l'utilisateur.
+ */
 @Component({
   selector: 'app-header',
   standalone: false,
@@ -9,18 +13,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  isLoggedIn = false;
-  userRole: string | null = null;
+
+  estConnecte: boolean = false;
+  roleUtilisateur: string | null = null;
+
   constructor(private authService: AuthService, private router: Router) {}
 
+  /**
+   * Initialise le composant en souscrivant aux changements d'état d'authentification.
+   */
   ngOnInit(): void {
-    this.authService.authStatus$.subscribe((isLoggedIn) => {
-      this.isLoggedIn = isLoggedIn;
-      this.userRole = this.authService.getUserRole();
+    this.authService.authStatus$.subscribe((estConnecte) => {
+      this.estConnecte = estConnecte;
+      this.roleUtilisateur = this.authService.getUserRole();
     });
   }
 
-  logout(): void {
+  /**
+   * Déconnecte l'utilisateur et le redirige vers la page d'accueil.
+   */
+  seDeconnecter(): void {
     this.authService.logout();
     this.router.navigate(['/']);
   }
